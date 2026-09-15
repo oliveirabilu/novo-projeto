5944d25adf356bb78bfd6f4435afb87746568df3
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-09-15 11:44:02 -0300

    chore: cria projeto base com Spring Initializr

---
+++ src/main/java/com/carlos/novo_projeto/NovoProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.novo_projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/novo_projeto/NovoProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.novo_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
